5e36ab6948c58b98967ae99a7c244acb254a864f
Pickle-Enjoyer <rileychoi20@gmail.com>
2025-05-10 17:41:31 -0400

    Part 2

--- Elephant.java
+++ Elephant.java
@@ -14,6 +14,8 @@ public class Elephant extends Actor
     
     
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    
     
     public Elephant()
     {
@@ -30,12 +32,21 @@ public class Elephant extends Actor
             idleLeft[i].scale(100, 100);
         }
         
+        animationTimer.mark();
+        
+        
         setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        
+        animationTimer.mark();
         if(facing.equals("right"))
         {
         setImage(idleRight[imageIndex]);
